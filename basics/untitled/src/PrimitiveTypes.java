d67a47fa26c28b91ac73d254752d4b14f5096358
tmatabane <thatomatabane241@hotmail.com>
2024-01-22 14:36:07 +0200

    Primitive types

---
+++ basics/untitled/src/PrimitiveTypes.java
@@ -0,0 +1,9 @@
+public class PrimitiveTypes {
+    public static void main(String[] args){
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+
+        System.out.println("Minimum value = "+minValue);
+        System.out.println("Maximum value = "+maxValue);
+    }
+}
