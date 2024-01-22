0432c8ca504f6ab45fd564f17c48159519304289
tmatabane <thatomatabane241@hotmail.com>
2024-01-22 14:49:05 +0200

    ByteShortIntLong

---
+++ basics/untitled/src/ByteShortIntLong.java
@@ -0,0 +1,23 @@
+public class ByteShortIntLong {
+    public static void main(String[] args){
+        byte minByteValue = Byte.MIN_VALUE;
+        byte maxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte value range: "+minByteValue+" , "+maxByteValue);
+
+        short minShortValue = Short.MIN_VALUE;
+        short maxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short value range: "+minShortValue+" , "+maxShortValue);
+
+        int minIntValue = Integer.MIN_VALUE;
+        int maxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Int value range: "+minIntValue+" , "+maxIntValue);
+
+        long minLongValue = Long.MIN_VALUE;
+        long maxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long value range: "+minLongValue+" , "+maxLongValue);
+    }
+}
