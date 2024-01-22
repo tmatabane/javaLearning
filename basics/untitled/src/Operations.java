ca779b2f95bf6512fef0839e91c0c10e58a1e1f9
tmatabane <thatomatabane241@hotmail.com>
2024-01-22 13:05:09 +0200

    operations

---
+++ basics/untitled/src/Operations.java
@@ -0,0 +1,10 @@
+public class Operations {
+    public static void main(String[] args){
+        int x = 65;
+        int y = 88;
+        int z;
+
+        z = x + y;
+        System.out.println(x+" + "+y+" = "+z);
+    }
+}
